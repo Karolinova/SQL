a5c421ed57b39ec513b7610e0dffa030c6520664
Karolinova <karolina.nowinska13@gmail.com>
2024-12-12 21:44:14 +0100

    in MSSQL

---
+++ Conditional selection of table to update/MSSQL.sql
@@ -0,0 +1,37 @@
+create procedure zakoncz_gre 
+	@game nvarchar(max), 
+	@data_zakonczenia date
+as
+begin 
+	declare @epic_tak bit;
+	declare @steam_tak bit;
+	declare @epic_id_key int;
+	declare @steam_id_key int;
+	-- verify, if game is on steam or epic
+	set @epic_tak = case when exists (select 1 from dbo.epic where game_epic_name = @game) then 1 else 0 end;
+	set @epic_id_key = (select epic_id from dbo.epic where game_epic_name = @game);
+	set @steam_tak = case when exists (select 1 from dbo.steam where game_steam_name = @game) then 1 else 0 end;
+	set @steam_id_key = (select steam_id from dbo.steam where game_steam_name = @game);
+	-- game is on epic
+	if @epic_tak = 1
+	begin
+		update dbo.epic_info 
+		set status = 'Ukończona'
+		, data_ukonczenia = @data_zakonczenia
+		where epic_id = @epic_id_key;
+		print 'Gra '+ @game +' z Epic została zakończona w dniu ' + CONVERT(NVARCHAR(10),@data_zakonczenia) ;
+	end
+	-- game is on steam
+	else if @steam_tak = 1
+	begin
+		update dbo.steam_info 
+		set status = 'Ukończona'
+		, data_ukonczenia = @data_zakonczenia
+		where steam_id = @steam_id_key;
+		print 'Gra '+ @game +' z Steam została zakończona w dniu ' + CONVERT(NVARCHAR(10),@data_zakonczenia) ;
+	end
+		else 
+		begin
+		print 'Brak gry w Epic i Steam';
+		end
+end;
