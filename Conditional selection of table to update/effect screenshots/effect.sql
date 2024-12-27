714ae8ba120a0d3b89b06935108c24ecfc2f1d7d
Karolinova <karolina.nowinska13@gmail.com>
2024-12-27 14:12:54 +0100

    add first execution a function

--- Conditional selection of table to update/effect screenshots/effect.sql
+++ Conditional selection of table to update/effect screenshots/effect.sql
@@ -1,7 +1,17 @@
 -- Exec procedure
 
+-- In MSSQL
+
 exec zakoncz_gre 'The Spirit and the Mouse','2024-11-25';
 -- Screenshot 'game_is' 
 
 exec zakoncz_gre 'The Spirit','2024-11-25';
 -- Screenshot 'no_game'
+
+
+-- Execute function
+
+-- In PostgreSQL
+
+select zakoncz_gre('The Spirit and the Mouse','2024-11-25');
+-- Screenshot 'game_is_pg'
