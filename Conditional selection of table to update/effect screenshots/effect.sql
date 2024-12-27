6819564fe42c22e33e9b7f47c9468dbcf9d7260c
Karolinova <karolina.nowinska13@gmail.com>
2024-12-27 14:15:39 +0100

    add second execution a function - 'no_game' option

--- Conditional selection of table to update/effect screenshots/effect.sql
+++ Conditional selection of table to update/effect screenshots/effect.sql
@@ -15,3 +15,6 @@ exec zakoncz_gre 'The Spirit','2024-11-25';
 
 select zakoncz_gre('The Spirit and the Mouse','2024-11-25');
 -- Screenshot 'game_is_pg'
+
+select zakoncz_gre('The Spirit','2024-11-25');
+-- Screenshot 'no_game_pg'
