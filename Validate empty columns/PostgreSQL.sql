31d7ee5711d8223565b0356c8bc42dfab8cb1d63
Karolinova <karolina.nowinska13@gmail.com>
2026-02-21 11:50:19 +0100

    in postgresql

---
+++ Validate empty columns/PostgreSQL.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION walidacja_pustych_kolumn (tabela text, variadic lista_kolumn text[])
+	RETURNS void
+	LANGUAGE plpgsql
+AS $function$
+DECLARE 
+	kol text;
+	brak_danych boolean;
+	lista_brakow text[];
+BEGIN
+	FOREACH kol IN ARRAY lista_kolumn LOOP
+		EXECUTE FORMAT(
+			$q$
+			SELECT ARRAY_AGG(DISTINCT nr_polisy) AS lista
+			FROM %I
+			WHERE %I IS NULL
+			$q$,
+			tabela,
+			kol
+		)
+		INTO lista_brakow;
+		IF lista_brakow IS NOT NULL THEN 
+			RAISE NOTICE 'Braki w kolumnie %: %', kol, lista_brakow;
+		ELSE
+			RAISE NOTICE 'Braki w kolumnie %.', kol;
+		END IF;
+	END LOOP;
+END;
+$function$;
