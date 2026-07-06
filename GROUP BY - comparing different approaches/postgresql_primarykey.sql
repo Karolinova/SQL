09e3d9b645f1e57d34bea152460575c20926d553
Karolinova <karolina.nowinska13@gmail.com>
2026-07-06 20:38:13 +0200

    add primary key solution in postgresql

---
+++ GROUP BY - comparing different approaches/postgresql_primarykey.sql
@@ -0,0 +1,12 @@
+select agr.imie, agr.nazwisko, agr.miasto
+, count(auwp.id_partii) as liczba_partii
+, count(auwp.id_partii) filter (where pozycja = 1) as liczba_zwyciestw
+, sum(ap.czas_trwania_gry) as laczny_czas_gry
+, array_agg(ap.id) as lista_partii
+, array_agg(distinct ag.nazwa_gry) as lista_gier
+, max(ap.data_rozgrywki) as ostatnia_gra
+from av_gracze agr
+left join av_udzial_w_partii auwp on auwp.id_gracza = agr.id
+left join av_partie ap on ap.id = auwp.id_partii
+left join av_gry ag on ag.id = ap.id_gry
+group by agr.id;
